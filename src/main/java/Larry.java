014b1bd4e8c14baf44a07935b30f20de4744595e
Ren <renfreddd@gmail.com>
2026-08-21 12:14:17 +0800

    Add Scanner and Input feature

--- src/main/java/Larry.java
+++ src/main/java/Larry.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Larry {
     public static void main(String[] args) {
         String banner = " _        _      ____    ____   __   __\n"
@@ -8,11 +10,19 @@ public class Larry {
 
         String line = "____________________________________________________________";
 
+        // initiate instance of scanner
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(line);
         System.out.print(banner);
+        System.out.println(line);
         System.out.println("Hello! I'm Larry! :)");
         System.out.println("What can I do for you?");
         System.out.println(line);
+
+        String input = scanner.nextLine();
+        System.out.println(input);
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
     }
