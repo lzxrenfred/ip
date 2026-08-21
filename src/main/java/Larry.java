26bff58ccc44cb30dd7971a23924a90539cbd13e
Ren <renfreddd@gmail.com>
2026-08-21 12:08:10 +0800

    Update Larry.java to greet users

--- src/main/java/Larry.java
+++ src/main/java/Larry.java
@@ -1,10 +1,19 @@
 public class Larry {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(banner);
+        String banner = " _        _      ____    ____   __   __\n"
+                + "| |      / \\    |  _ \\  |  _ \\  \\ \\ / /\n"
+                + "| |     / _ \\   | |_) | | |_) |  \\ V / \n"
+                + "| |___ / ___ \\  |  _ <  |  _ <    | |  \n"
+                + "|_____/_/   \\_\\ |_| \\_\\ |_| \\_\\   |_|  \n";
+
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.print(banner);
+        System.out.println("Hello! I'm Larry! :)");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
