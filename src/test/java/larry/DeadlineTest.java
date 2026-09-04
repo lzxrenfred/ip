d9eb54d91bc9cf44c5a4ba22b9460490fb2a7fbc
Ren <e1399061@u.nus.edu>
2026-09-04 10:00:19 +0800

    Add JUnit tests

---
+++ src/test/java/larry/DeadlineTest.java
@@ -0,0 +1,25 @@
+package larry;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+public class DeadlineTest {
+
+    @Test
+    public void toString_validDate_formatsDateCorrectly() {
+        Deadline deadline = new Deadline("submit assignment", "2026-09-10");
+        assertEquals(
+                "[D][ ] submit assignment (by: Sept 10 2026)",
+                deadline.toString()
+        );
+    }
+
+    @Test
+    public void markAsDone_deadline_updatesStatusAndKeepsDate() {
+        Deadline deadline = new Deadline("submit assignment", "2026-09-10");
+        deadline.markAsDone();
+        assertEquals(
+                "[D][X] submit assignment (by: Sept 10 2026)",
+                deadline.toString()
+        );
+    }
+}
---
+++ src/test/java/larry/TaskTest.java
@@ -0,0 +1,21 @@
+package larry;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+public class TaskTest {
+
+    @Test
+    public void markAsDone_taskNotDone_changesStatusToDone() {
+        Task task = new Todo("read book");
+        task.markAsDone();
+        assertEquals("[T][X] read book", task.toString());
+    }
+
+    @Test
+    public void markAsNotDone_taskDone_changesStatusToNotDone() {
+        Task task = new Todo("read book");
+        task.markAsDone();
+        task.markAsNotDone();
+        assertEquals("[T][ ] read book", task.toString());
+    }
+}
